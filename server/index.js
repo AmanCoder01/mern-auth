dfed306746b7852557cc1985291aee065caecac0
Aman Maurya <114045665+AmanCoder01@users.noreply.github.com>
2024-08-10 15:47:08 +0530

    changes commit

--- server/index.js
+++ server/index.js
@@ -1,17 +1,23 @@
 import express from "express";
 import dotenv from "dotenv";
 import cors from "cors";
-import { connectDb } from "./db/connectDb.js";
-import authRoutes from "./routes/auth.route.js"
+import cookieParser from "cookie-parser";
 import path from "path";
 
+
+import authRoutes from "./routes/auth.route.js";
+import { connectDb } from "./db/connectDb.js";
+
 dotenv.config();
+
 const app = express();
+const PORT = process.env.PORT || 5000;
 const __dirname = path.resolve();
 
 app.use(cors({ origin: "http://localhost:5173", credentials: true }));
-app.use(express.json()); //allow us to parse req.body
 
+app.use(express.json()); // allows us to parse incoming requests:req.body
+app.use(cookieParser()); // allows us to parse incoming cookies
 
 app.use("/api/auth", authRoutes);
 
@@ -23,8 +29,7 @@ if (process.env.NODE_ENV === "production") {
     });
 }
 
-
-app.listen(process.env.PORT, () => {
+app.listen(PORT, () => {
     connectDb();
-    console.log(`Server is running on PORT ${process.env.PORT}`);
-})
+    console.log("Server is running on port: ", PORT);
+});
